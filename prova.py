1af4ba842861d6e3ee718d94e8f09ac9df0623c3
lindattt <>
2021-12-10 10:34:30 +0000

    Nuovi file

---
+++ prova.py
@@ -0,0 +1,17 @@
+import math
+
+print("prova")
+
+a = 2.1
+b = 3
+r = 2 + 2j
+print(type(r))
+
+
+print(a)
+
+
+p = math.pi
+
+print(p)
+print(math.pi)
---
+++ strings.py
@@ -0,0 +1,5 @@
+a="""I am a string spanning in 3 rows, ... containing 'sigle quotes',
+... containing "double quotes",
+... containing '''triple quotes'''"""""
+
+print(a)
